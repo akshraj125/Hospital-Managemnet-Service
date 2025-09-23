7076032763ed4710f84ebc38f98b7feef9577c26
Akshay <akshraj125@gmail.com>
2025-09-23 18:41:53 +0530

    db name updated to hospital

--- src/HospitalManagementSystem/HospitalManagementSystem.java
+++ src/HospitalManagementSystem/HospitalManagementSystem.java
@@ -4,7 +4,7 @@ import java.sql.*;
 import java.util.Scanner;
 
 public class HospitalManagementSystem {
-    private static final String url = "jdbc:mysql://localhost:3306/";
+    private static final String url = "jdbc:mysql://localhost:3306/hospital";
     private static final String username = "root";
     private static final String password = "root";
 
